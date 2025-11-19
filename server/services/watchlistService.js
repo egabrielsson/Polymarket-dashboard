34cd27d95625d95deceed237a0346037d0dbb142
Daniel Norberg <danno@student.chalmers.se>
2025-11-19 19:23:07 +0100

    Add ObjectId validation to service (#10)

--- server/services/watchlistService.js
+++ server/services/watchlistService.js
@@ -8,7 +8,17 @@ module.exports = {
   //Add a market to a user's watchlist.
 
   async addToWatchlist(userId, marketId) {
-    
+     if (!mongoose.Types.ObjectId.isValid(userId)) {
+      const err = new Error('Invalid userId');
+      err.code = 'BAD_REQUEST';
+      throw err;
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(marketId)) {
+      const err = new Error('Invalid marketId');
+      err.code = 'BAD_REQUEST';
+      throw err;
+    }
     throw new Error('Not implemented: addToWatchlist');
   }
 };
