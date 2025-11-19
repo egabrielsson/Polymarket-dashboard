92c44ccb5b80b791349b313d527fa3d1076b40eb
Daniel Norberg <danno@student.chalmers.se>
2025-11-19 19:13:21 +0100

    Add service scaffold for watchlist (#10)

---
+++ server/services/watchlistService.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+// const User = require('../models/User');
+// const Market = require('../models/Market');
+// const Watchlist = require('../models/Watchlist');
+
+module.exports = {
+  
+  //Add a market to a user's watchlist.
+
+  async addToWatchlist(userId, marketId) {
+    
+    throw new Error('Not implemented: addToWatchlist');
+  }
+};
