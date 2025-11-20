bc57b4007fca12ebb0cb8bd1098ef4442aa97f7e
Daniel Norberg <danno@student.chalmers.se>
2025-11-20 15:05:45 +0100

    Add GET /users/:userId/watchlist route (#12)

--- server/routes/watchlist.js
+++ server/routes/watchlist.js
@@ -4,13 +4,17 @@ const express = require('express'); // loads the express package
 const router = express.Router();
 
 // 
-const { addToWatchlist } = require('../controllers/watchlistController'); 
+const { addToWatchlist, getUserWatchlist } = require('../controllers/watchlistController'); 
 
 // POST /api/users/:userId/watchlist
 // and adds a market to the user's watchlist.
 // Returns with 201 with updated watchlist on success.
 // Returns 409 if the market is already in the watchlist.
 router.post('/users/:userId/watchlist', addToWatchlist);
+
+// GET /api/users/:userId/watchlist
+// Returns array of populated markets
+router.get('/users/:userId/watchlist', getUserWatchlist);
 
 // exports the router to be used later.
 module.exports = router;
