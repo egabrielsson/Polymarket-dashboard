a9de52d79846dbe1b1e69c5e271ca6ae30723bbc
erikgabr <erikgabr@student.chalmers.se>
2025-12-01 04:51:24 +0100

    Register browse markets route (#28)

--- client/src/router.js
+++ client/src/router.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from './views/Home.vue'
+import BrowseMarkets from './views/browseMarkets.vue'
 
 const routes = [
-  { path: '/', name: 'home', component: Home }
+  { path: '/', name: 'home', component: Home },
+  { path: '/markets', name: 'browseMarkets', component: BrowseMarkets }
 ]
 
 const router = createRouter({
