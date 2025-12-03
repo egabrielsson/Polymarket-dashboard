cfd44105052d3c8dc30606383fc573768a54e869
erikgabr <erikgabr@student.chalmers.se>
2025-12-03 02:07:42 +0100

    Extend router for sidebar views (#30)

--- client/src/router.js
+++ client/src/router.js
@@ -2,10 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from './views/Home.vue'
 import BrowseMarkets from './views/browseMarkets.vue'
+import Account from './views/Account.vue'
+import WatchList from './views/WatchList.vue'
 
 const routes = [
-  { path: '/', name: 'home', component: Home },
-  { path: '/markets', name: 'browseMarkets', component: BrowseMarkets }
+  { path: '/', redirect: '/markets' },
+  { path: '/home', name: 'home', component: Home },
+  { path: '/account', name: 'account', component: Account },
+  { path: '/markets', name: 'browseMarkets', component: BrowseMarkets },
+  { path: '/watchlist', name: 'watchlist', component: WatchList }
 ]
 
 const router = createRouter({
