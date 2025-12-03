ce93248f203fdf0d873e1ff932d430a9e1c58e6e
erikgabr <erikgabr@student.chalmers.se>
2025-12-03 02:07:37 +0100

    Update App layout for sidebar (#30)

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,41 @@
 <template>
-  <div id="app">
-    <!-- Temporarily focusing on the Browse Markets experience 
-    we just add the other views that we want to display here
-    -->
-    <BrowseMarkets />
-    <!-- <router-view/> -->
+  <div id="app" class="container-fluid p-0">
+    <div class="row g-0 min-vh-100 flex-md-nowrap">
+      <div class="col-12 col-md-3 col-xl-2">
+        <SideBar />
+      </div>
+      <main class="col bg-light py-4 px-3 px-md-5">
+        <router-view />
+      </main>
+    </div>
   </div>
 </template>
 
 <script>
-import BrowseMarkets from './views/browseMarkets.vue'
+import SideBar from './components/SideBar.vue'
 
 export default {
   name: 'App',
   components: {
-    BrowseMarkets
+    SideBar
   }
 }
 </script>
 
 <style>
+:root {
+  --poly-blue: #2563eb;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #1f2933;
+  background-color: #f5f7fb;
+}
+
+main {
+  min-height: 100vh;
 }
 </style>
