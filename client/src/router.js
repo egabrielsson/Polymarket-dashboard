af6bb055b261582ed0a0aa1ee3ae2353596aa835
erikgabr <erikgabr@student.chalmers.se>
2025-12-05 03:53:17 +0100

    feat: Integrate session store and resolve merge conflicts

--- client/src/router.js
+++ client/src/router.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Login from './views/Login.vue'
 import Home from './views/Home.vue'
-import BrowseMarkets from './views/browseMarkets.vue'
+import BrowseMarkets from './views/BrowseMarkets.vue'
 import Account from './views/Account.vue'
 import WatchList from './views/WatchList.vue'
 import { getStoredSession } from './stores/sessionStore'
